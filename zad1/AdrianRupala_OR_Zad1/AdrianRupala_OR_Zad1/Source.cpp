0c881e3ec737f2dd93771164eb04bc1a1ba0dd38
adrixop95 <adrixop95@me.com>
2018-11-11 23:04:56 +0100

    lepsze komentarze chyba

--- zad1/AdrianRupala_OR_Zad1/AdrianRupala_OR_Zad1/Source.cpp
+++ zad1/AdrianRupala_OR_Zad1/AdrianRupala_OR_Zad1/Source.cpp
@@ -7,6 +7,7 @@ using namespace std;
 // Jakikolwiek tutorial lepszy ni¿ te slajdy: https://iop.vast.ac.vn/~nvthanh/cours/parcomp/Running%20C%20Program%20in%20parallel%20using%20MPI.pdf
 // https://computing.llnl.gov/tutorials/mpi/
 
+// Funkcja z zadania
 int f(int i) {
 	int i4_huge = 2147483647;
 	int j, k, value = i;
@@ -28,7 +29,7 @@ void sprawdzenie(int c) {
 	// Chyba poprawne sprawdzenie zgodnie z za³o¿eniem f(j) == c dla liczby 2000000000
 	// Nie mam pojêcia :C
 	for (int j = 0; j < 2000000000; j++) {
-		//cout << f(j) << endl;
+		//cout << f(j) << endl; //Sprawdzanie czy to wgl dzia³a xd
 
 		if (f(j) == c) {
 			cout << "Znalazlem! " << f(j) << endl;
@@ -60,14 +61,12 @@ int main(int argc, char* argv[]) {
 
 	*/
 	
-	MPI_Init(&argc, &argv);
+	MPI_Init(&argc, &argv); // Start obliczeñ MPI
 
-	int moj_nr, p, c, tag; // Moj_nr -> numer procesu, p -> liczba procesów, c -> liczba od urzytkownika
+	int moj_nr, p, c; // Moj_nr -> numer procesu, p -> liczba procesów, c -> liczba od urzytkownika
 	int counter = 0; // Counter -> liczba wyst¹pieñ danej liczby
-	int best_value = 0;
-	int best_of_all;
 
-	MPI_Status status;
+	MPI_Status status;  // nie wiem po co to jest ale by³o w examplach
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &moj_nr); // Odczytaj numer procesu
 	MPI_Comm_size(MPI_COMM_WORLD, &p); // Odczytaj liczbê procesoów
@@ -80,24 +79,20 @@ int main(int argc, char* argv[]) {
 	}
 	MPI_Bcast(&c, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	
-	if (moj_nr < p) {
-
+	if (moj_nr < p) {		
+		auto start = chrono::high_resolution_clock::now(); //Liczenie czasu start
+		sprawdzenie(c); // funkcja z zadania 
+		auto finish = chrono::high_resolution_clock::now(); //Liczenie czasu stop
 		
-		auto start = chrono::high_resolution_clock::now();
-		sprawdzenie(c);
-		auto finish = chrono::high_resolution_clock::now();
-		
-		chrono::duration<double> elapsed = finish - start;
+		chrono::duration<double> elapsed = finish - start; //czas ca³kowity + print
 		cout << "Czas: " << elapsed.count() << "s\n";
 		// Wynik dla i7-8700/16gb ram, liczba c = 1229094172
 		// 2 procesy -> Czas: 120.577s
 		// 4 procesy -> takie same wyniki, bez sensu
-		// 6 procesów -> takie same wyniki, bez sensu
-		
+		// 6 procesów -> takie same wyniki, bez sensu		
 	}
 
 
-	MPI_Finalize();
-	
+	MPI_Finalize();	// Koniec obliczeñ MPI
 	return 0;
 }
