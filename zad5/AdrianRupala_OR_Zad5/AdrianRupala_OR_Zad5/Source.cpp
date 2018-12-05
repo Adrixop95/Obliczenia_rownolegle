ec9dc0689c2a80c02e4ae1fa260fcfab12c1babb
adrixop95 <adrixop95@me.com>
2018-12-05 17:09:21 +0100

    zad5

--- zad5/AdrianRupala_OR_Zad5/AdrianRupala_OR_Zad5/Source.cpp
+++ zad5/AdrianRupala_OR_Zad5/AdrianRupala_OR_Zad5/Source.cpp
@@ -9,10 +9,11 @@ int moj_nr, p; // globalne zmiennie dt. MPI
 
 // z³o¿onoœæ tej funkcji jest kwadratowa, to obliczanie bêdzie trwa³o wiecznoœæ na moim sprzêcie, nie podejmujê siê tego, INT_MAX (32bit) to 2147483647, mo¿na u¿yæ _I64_MAX (64 bit) co daje jeszcze wiêkszy zakres 9223372036854775807
 void calculate() {
-	for (int x = 0; x <= INT_MAX; x++) { // liczby rzeczywiste x dodatnie od 0 do MAX_INT32 w c++ 2147483647
+	for (int x = 1; x <= INT_MAX; x++) { // liczby rzeczywiste x dodatnie od 0 do MAX_INT32 w c++ 2147483647
 		if (x % p == moj_nr) { // podzia³ na w¹tki MPI
-			for (int y = 0; y <= INT_MAX; y++) { // liczby rzeczywiste y dodatnie od 0 do MAX_INT32 w c++ 2147483647
-				if (x - y < 2 && 2 * x + 3 * y < 1 && (0.2 - y) / (1.3 - x) - sin(x) / x < -1.1008) { // warunek z zadania
+			//for (int y = 0; y <= INT_MAX; y++) { // liczby rzeczywiste y dodatnie od 0 do MAX_INT32 w c++ 2147483647
+			for (int y = 1; y < x + 2; y++){
+				if (y - x < 2 && 2 * x + 3 * y < 1 && (0.2 - y) / (1.3 - x) - sin(x) / x < -1.1008) { // warunek z zadania
 					cout << "x: " << x << ", y: " << y << endl; // wypisz liczby
 				}
 			}
