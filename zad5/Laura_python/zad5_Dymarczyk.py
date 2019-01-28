b487d4520e40499e2cb135048af72a97b29bf15f
adrixop95 <adrixop95@me.com>
2019-01-28 10:43:23 +0100

    Changes range in loops in exer. 5 in python

--- zad5/Laura_python/zad5_Dymarczyk.py
+++ zad5/Laura_python/zad5_Dymarczyk.py
@@ -8,13 +8,21 @@ rank = comm.Get_rank()
 ret = []
 time = MPI.Wtime()
 
-i4_huge = 2*(10**5)
 
+x=0.0000001
+y=0.0000001
+x_add=x
+y_add=y
 
-for x in range(rank*i4_huge//size, (rank+1)*i4_huge//size):
-    for y in range(1, x+2):
-        if 2*x+3*y < 1 and (0.2-y)/(1.3-x)-sin(x)/x < -1.1008:
+while x<1/3:
+    while y<1/3:
+        if (0.2-y)/(1.3-x)-sin(x)/x < -1.1008:
             ret += (x, y)
+            x, y = 1, 1
+        y+=y_add
+    x+=x_add
+        
+
 
 print('rank', rank, 'has data:', ret)
 print('time', time, 'seconds')
