16d410d49e6177a452924a9363b9423af8f1cdef
adrixop95 <adrixop95@gmail.com>
2019-01-29 08:06:40 +0100

    Reapered exer. 4 in python

--- zad4/DymarczykLaura/hash.py
+++ zad4/DymarczykLaura/hash.py
@@ -3,7 +3,7 @@ def hash(command, c):
     for i in command:
         x <<= 3
         x ^= ord(i)
-    return int((c/x)%41)
+    return int((c / x) % 41)
 
 
 commands = ["and", "del", "from", "not", "while",
@@ -14,8 +14,9 @@ commands = ["and", "del", "from", "not", "while",
             "continue", "finally", "is", "return",
             "def", "for", "lambda", "try"]
 
+
 def hashuj_tablice(tablica, c):
-    ret=[]
+    ret = []
     for komenda in tablica:
         ret.append(hash(komenda, c))
     return ret
@@ -23,16 +24,26 @@ def hashuj_tablice(tablica, c):
 
 def sprawdz_unikalnosc(zahashowana_tablica):
     for i in zahashowana_tablica:
-        if (zahashowana_tablica.count(i)!=1):
+        if (zahashowana_tablica.count(i) != 1):
             return False
     return True
 
-def zadanie_4(komendy, zakres_do, zakres_od=0):
-    for i in range(zakres_od, zakres_do+1):
-        zahaszowana = hashuj_tablice(commands, i)
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+
+def zadanie_4(komendy):
+    i4_huge = 2 * (10 ** 9)
+    for i in range(rank * i4_huge // size, (rank + 1) * i4_huge // size):
+        zahaszowana = hashuj_tablice(komendy, i)
         if sprawdz_unikalnosc(zahaszowana):
-            return c
+            print(i)
+            return i
     return 0
-        
-    
-print(zadanie_4(commands, 10**11, int(9.9999*(10**10))))
+
+
+zadanie_4(commands)
