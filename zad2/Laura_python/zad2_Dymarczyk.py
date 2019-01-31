431436ec0348a5bf855a31d0d552f9f9e0bbcb0e
Laura <laura.dymarczyk@smcebi.edu.pl>
2019-02-01 00:15:58 +0100

    Added MPI in exerc 2

--- zad2/Laura_python/zad2_Dymarczyk.py
+++ zad2/Laura_python/zad2_Dymarczyk.py
@@ -1,12 +1,12 @@
 from mpi4py import MPI
 import copy
 
-print('cos')
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
+time = MPI.Wtime()
 
 def tworz_plansze(n):
     ret = []
@@ -45,7 +45,7 @@ def ile_sasiadow(plansza, x, y, n):
 def krok(plansza, n):
     kopia = copy.deepcopy(plansza)
     for i in range(n):
-     for j in range(n):
+     for j in range(rank*n//size, (rank+1)*n//size):
          if ile_sasiadow(kopia, i, j, n) in [2, 1]:
              plansza[i][j] = 1
          else:
@@ -63,9 +63,9 @@ def generuj_string(plansza, n):
 
 def zadanie_2(n, k):
     """
-    Rozwiązanie zadania 2
+    Rozwiazanie zadania 2
     :param n: wymiar macierzy
-    :param k: liczba kroków
+    :param k: liczba krokow
     wynik zapisany do pliku
     """
     plansza = tworz_plansze(n)
@@ -74,12 +74,11 @@ def zadanie_2(n, k):
     plik = open('wynik_zadania2', 'w')
     plik.write(generuj_string(plansza, n))
     plik.close()
+    return plansza
 
 
-plansza = tworz_plansze(10)
-for i in range(10):
-    print(plansza[i])
-print()
-plansza = krok(plansza, 10)
-for i in range(10):
-    print(plansza[i])
+plansza = zadanie_2(8000, 400)
+
+time = MPI.Wtime() - time
+
+print('rank', rank, ' has time: ', time, 'seconds')
